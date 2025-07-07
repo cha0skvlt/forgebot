d6f71ef561a91a7edafd3317fb08c50b3b263272
cha0skvlt <cha0skvlt@gmail.com>
2025-07-07 12:26:18 +0300

    Add production Docker setup

---
+++ modules/env.py
@@ -0,0 +1,13 @@
+import os
+
+
+def get_env(name: str, default: str | None = None, *, required: bool = False) -> str | None:
+    value = os.getenv(name)
+    if value:
+        return value
+    if default is not None:
+        return default
+    if required:
+        raise RuntimeError(f"{name} not set")
+    return None
+
